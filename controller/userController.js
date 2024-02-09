a2378d6d9f6a9288231f3f935ec48c337dfb91da
Tomasz Pawlikowski <tp4458@gmail.com>
2024-02-09 23:14:49 +0000

    controllers - get all users

--- controller/userController.js
+++ controller/userController.js
@@ -1,1 +1,18 @@
 const { Users } = require('../models');
+
+const userControllers = {
+  //GET all users:
+  getAllUsers(req, res) {
+    Users.find({})
+      .populate({ path: 'thoughts', select: '-__v' })
+      .populate({ path: 'friends', select: '-__v' })
+      .select('-__v')
+      .then((dbUsersData) => res.json(dbUsersData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+}; //END BRACKET
+
+module.exports = userControllers;
