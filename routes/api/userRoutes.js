e886003439a3d02bb2a3bd07c99b804ba90203f9
Tomasz Pawlikowski <tp4458@gmail.com>
2024-02-09 23:54:36 +0000

    user routes updated

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,9 @@
 const router = require('express').Router();
+
+const { getAllUsers, getOneUser } = require('../../controller/userController');
+
+router.route('/').get(getAllUsers);
+
+router.route('/:id').get(getOneUser);
+
+module.exports = router;
