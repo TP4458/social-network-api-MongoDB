bf7c6e76a2a5caf3ad4d80a71fe9f0c68ee048be
Tomasz Pawlikowski <tp4458@gmail.com>
2024-02-09 01:29:28 +0000

    user model

---
+++ model/Users.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+const { userInfo } = require('os');
+const { isEmail } = require('validator');
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: [isEmail, 'Invalid email'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thoughts',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Users',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+//friend count
+UserSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const Users = model('Users', UserSchema);
+
+module.exports.Users;
