95ea84472a00c44ae8ef2438fe2c2dc6f0f0b25c
Tomasz Pawlikowski <tp4458@gmail.com>
2024-02-11 00:12:16 +0000

    delete thought controller

--- controller/thoughtsController.js
+++ controller/thoughtsController.js
@@ -45,14 +45,41 @@ const thoughtsControllers = {
       })
       .then((dbThoughtsData) => {
         if (!dbThoughtsData) {
-          res
-            .status(404)
-            .json({ message: 'No thoughts with this particular ID!' });
+          res.status(404).json({ message: 'no thought found with this ID' });
           return;
         }
         res.json(dbThoughtsData);
       })
       .catch((err) => res.json(err));
+  },
+
+  updateThought({ params, body }, res) {
+    Thoughts.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .populate({ path: 'reactions', select: '-__v' })
+      .select('-___v')
+      .then((dbThoughtsData) => {
+        if (!dbThoughtsData) {
+          res.status(404).json({ message: 'no thought found with this ID' });
+          return;
+        }
+        res.json(dbThoughtsData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  deteteThought({ params }, res) {
+    Thoughts.findOneAndDelete({ _id: params.id })
+      .then((dbThoughtsData) => {
+        if (!dbThoughtsData) {
+          res.status(404).json({ message: 'no thought found with this ID' });
+          return;
+        }
+        res.json(dbThoughtsData);
+      })
+      .catch((err) => res.status(400).json(err));
   },
 }; //end bracket
 
--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -4,9 +4,10 @@ const {
   getAllThoughts,
   getOneThought,
   newThought,
+  updateThought,
 } = require('../../controller/thoughtsController');
 
 router.route('/').get(getAllThoughts).post(newThought);
-router.route('/:id').get(getOneThought);
+router.route('/:id').get(getOneThought).put(updateThought);
 
 module.exports = router;
